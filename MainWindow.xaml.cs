e015a1a0e40c39f42efeb8d6d3e69e23ba0c00af
Axner590 <kalininalexey2017@yandex.ru>
2022-02-16 00:52:00 +0300

    Changed the variable

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace San4oClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        Boolean githubTest = false;
+        Boolean githubTest = true;
         public MainWindow()
         {
             InitializeComponent();
